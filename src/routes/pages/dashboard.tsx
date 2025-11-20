92f4347966e645c6396064484ab85fd768619793
Roberto 'Andler' Lucas <contact@andler.dev>
2025-11-20 01:35:06 -0600

    chore: rm build files

--- src/routes/pages/dashboard.tsx
+++ src/routes/pages/dashboard.tsx
@@ -6,8 +6,8 @@ import { dashboardRouteOptions, getDashboardDataHandler } from '~/lib/server/das
 
 export const dashboardPageRoutes = new Elysia()
   .get('/', (response) => {
-    // TODO: Continue from here... need to fix how to send data from server to components.
-    console.log('response data', JSON.stringify(response))
+    // TODO: Continue from here... need to fix how to send data from server to components. The data is not returning as expecting with their functions and variables.
+    console.log('response data', JSON.stringify(response, null, 2))
     const dashboardData = getDashboardDataHandler(response);
     return renderPage(
       'AgentCanvas - Dashboard',
